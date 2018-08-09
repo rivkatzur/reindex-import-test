3cc7fb8cdf55bf4952ba066664b28bd966049e00
Rivka Tzur <rivkatzur@users.noreply.github.com>
2018-08-09 18:36:38 +0300

    Update consumer.js

--- consumer.js
+++ consumer.js
@@ -1,11 +1,12 @@
 
+
 'use strict';
 
 var ImportRecordsCtrl = require('./controller');
 var importRecordsCtrl = new ImportRecordsCtrl();
-var fs = require('fs');
-var path = require("path");
-var inputPath = path.resolve(config.root, 'files');
+// var fs = require('fs');
+// var path = require("path");
+// var inputPath = path.resolve(config.root, 'files');
 
 module.exports = function(rabbit, qData) {
     rabbit.consume(qData.name, qData.maxUnackMessages, handleMessage);
@@ -16,21 +17,19 @@ function handleMessage(message, error, done) {
 }
 
 function importRecords(message, error, done) {
-    importRecordsCtrl.getResults(message.i).then(trials => {
-        let sourceFile = `${inputPath}/records${message.i}.csv`;
+    // return done()
+    importRecordsCtrl.getResults(message.i).then(result => {
+        // let sourceFile = `${inputPath}/records${message.i}.csv`;
 
-        fs.writeFile(sourceFile, trials, function(err) {
-            if(err) {
-                return console.log(err);
-            }
+        // fs.writeFile(sourceFile, JSON.stringify(trials), function(err) {
+        //     if(err) {
+        //         return console.log(err);
+        //     }
 
-            console.log("The file was saved!");
+        //     console.log("The file was saved!");
             done();
-            if (message.i >= parseInt(message.count)/100)
+            // if (message.i >= parseInt(message.count)/100)
                 importRecordsCtrl.emitter.emit('finish-reindex-import-from-api');
-        }); 
-    }).catch(err => {
-        console.log('------------------', err, message);
-        done();
+        // }); 
     });
 }
